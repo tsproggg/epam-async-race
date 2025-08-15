c616b4af97b997a5408c74738ad1cc80c44abbd0
TS prog <sofyan-w@mail.ru>
2025-08-15 10:31:53 +0300

    chore: installed tailwindcss, axios; downloaded given api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-    return (
-        <>
-            1
-        </>
-    )
+import React from "react";
+
+function App(): React.ReactNode {
+  return (
+    <div className="text-xl text-yellow-500">
+      <h1>Test</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
