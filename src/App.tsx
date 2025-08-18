b1beca660a3d18f6e4f16be33a496e7af60c044c
TS prog <sofyan-w@mail.ru>
2025-08-18 22:32:06 +0300

    fix: moved store Provider to main.tsx so it can cover main component of application too

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 
 import routes from "./routes";
-import store from "./store/store";
 
 function App(): React.ReactNode {
-  return (
-    <BrowserRouter>
-      <Provider store={store}>{routes()}</Provider>
-    </BrowserRouter>
-  );
+  return <BrowserRouter>{routes()}</BrowserRouter>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import "./index.css";
+import store from "./store/store";
 
 const root: HTMLElement | null = document.getElementById("root");
 if (root) {
   createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   );
 } else {
