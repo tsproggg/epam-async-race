aed929fbc29dca3582468817845fee0106c8b697
TS prog <sofyan-w@mail.ru>
2025-08-18 12:32:23 +0300

    fix: added missing page name to the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,14 @@ import { Links } from "../Links";
 
 export default function Header(): React.ReactNode {
   return (
-    <header className={"bg-red-300 h-64"} id="header">
+    <header className={"bg-red-300 h-64 flex justify-around"} id="header">
+      <h1 className={"flex items-center justify-around font-bold text-xl"}>
+        {window.location.pathname === Links.GARAGE ? "Garage" : "Winners"}
+      </h1>
       <nav id="navbar">
         <ul
           className={
-            "h-64 flex justify-around items-center text-xl font-bold [&>li]:hover:underline"
+            "h-64 flex items-center text-xl gap-15 [&>li]:hover:underline"
           }
         >
           <li>
