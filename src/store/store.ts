45f3c262150641ae8d5a1d0b2867283dae478606
TS prog <sofyan-w@mail.ru>
2025-08-18 22:28:17 +0300

    fix: added missing slices exports in store.ts, added types exports for react-redux hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -16,4 +16,8 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export default store;
+export { GarageSlice, WinnersSlice };
