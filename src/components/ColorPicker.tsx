8c6885b4358fe052163aef55c13e5987a46ec08a
TS prog <sofyan-w@mail.ru>
2025-08-22 09:35:08 +0300

    feat: finished color picker feature

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,21 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { HexColorPicker } from "react-colorful";
+import { useDispatch, useSelector } from "react-redux";
 
 import Popup from "./Popup";
+import { setColor } from "../store/CarPropsInputBufferSlice";
+
+import type { RootState } from "../store/store";
 
 export default function ColorPicker(): React.ReactNode {
-  const [color, setColor] = useState<string>("#000000");
+  const color: string = useSelector(
+    (state: RootState) => state.carPropsInputBufferSlice.color,
+  );
+  const dispatch = useDispatch();
 
   return (
     <Popup
-      openPopupComponent={<span>Open Color Picker</span>}
       popupName={"Color picker"}
+      openPopupComponent={
+        <div
+          className="grow-1 w-30 h-30 rounded-2xl"
+          id={"coloredBox"}
+          style={{ backgroundColor: color }}
+        />
+      }
       popupChildren={
-        <React.Fragment>
-          <p>Color: {color}</p>
-          <HexColorPicker onChange={(e) => setColor(e)} />
-        </React.Fragment>
+        <div className={"flex justify-center gap-15"} id={"colorPicker"}>
+          <HexColorPicker
+            color={color}
+            onChange={(newColor: string) => {
+              dispatch(setColor(newColor));
+            }}
+          />
+          <div
+            className="grow-1 w-30 rounded-2xl"
+            id={"coloredBox"}
+            style={{ backgroundColor: color }}
+          />
+        </div>
       }
       position={{
         x: 0,
