ce2b7b9a44d0900fb625e83e2cf4974c7edb182e
TS prog <sofyan-w@mail.ru>
2025-08-22 16:09:46 +0300

    feat: added AutoPopup component (a popup opening on spawn), refactored Popup component such as it is based on AutoPopup

---
+++ src/components/AutoPopup.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export interface AutoPopupProps {
+  popupName: string;
+
+  // If center is true, it will override coords
+  position: { x: number; y: number; center: boolean };
+  popupChildren: React.ReactNode;
+}
+
+// A popup opening automatically on spawn
+export default function AutoPopup(props: AutoPopupProps): React.ReactNode {
+  const [isOpened, setIsOpened] = useState<boolean>(true);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const { popupName, popupChildren, position } = props;
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (isOpened) {
+      if (!dialog.open) {
+        dialog.showModal();
+      }
+    } else if (dialog.open) {
+      dialog.close();
+    }
+  }, [isOpened]);
+
+  if (!isOpened) {
+    return null;
+  }
+
+  // TODO: add popup close on backdrop click
+  return (
+    <div>
+      <dialog
+        ref={dialogRef}
+        id={"autoPopup"}
+        onCancel={() => setIsOpened(false)}
+        className={`absolute
+            ${
+              position.center
+                ? "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                : ""
+            }
+            bg-white p-20 rounded-xl min-w-[300px] text-center backdrop:bg-black/50`}
+        style={
+          !position.center
+            ? { top: position.y, left: position.x, position: "absolute" }
+            : {}
+        }
+      >
+        <div className={"flex justify-around mb-10"} id="popupHeader">
+          <h5 className={"font-bold text-xl"}>{popupName}</h5>
+          <button
+            id="closePopup"
+            onClick={() => setIsOpened(false)}
+            type="button"
+          >
+            Close
+          </button>
+        </div>
+        <div className={"flex justify-center items-center"}>
+          <div>{popupChildren}</div>
+        </div>
+      </dialog>
+    </div>
+  );
+}
--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,33 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
-interface PopupProps {
-  popupName: string;
+import AutoPopup from "./AutoPopup";
 
-  // If center is true, it will override coords
-  position: { x: number; y: number; center: boolean };
-  popupChildren: React.ReactNode;
+import type { AutoPopupProps } from "./AutoPopup";
+
+interface PopupProps extends AutoPopupProps {
   openPopupComponent: React.ReactNode;
 }
 
 export default function Popup(props: PopupProps): React.ReactNode {
   const [isOpened, setIsOpened] = useState<boolean>(false);
-  const dialogRef = useRef<HTMLDialogElement>(null);
-  const { openPopupComponent, popupName, popupChildren, position } = props;
+  const { openPopupComponent } = props;
 
-  useEffect(() => {
-    const dialog = dialogRef.current;
-    if (!dialog) return;
-
-    if (isOpened) {
-      if (!dialog.open) {
-        dialog.showModal();
-      }
-    } else if (dialog.open) {
-      dialog.close();
-    }
-  }, [isOpened]);
-
-  // TODO: add popup close on backdrop click
   return (
     <div id={"popup"}>
       <button
@@ -38,37 +22,7 @@ export default function Popup(props: PopupProps): React.ReactNode {
         {openPopupComponent}
       </button>
 
-      <dialog
-        ref={dialogRef}
-        id={"popup-opened"}
-        onCancel={() => setIsOpened(false)}
-        className={`absolute
-            ${
-              position.center
-                ? "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                : ""
-            }
-            bg-white p-20 rounded-xl min-w-[300px] text-center backdrop:bg-black/50`}
-        style={
-          !position.center
-            ? { top: position.y, left: position.x, position: "absolute" }
-            : {}
-        }
-      >
-        <div className={"flex justify-around mb-10"} id="popupHeader">
-          <h5 className={"font-bold text-xl"}>{popupName}</h5>
-          <button
-            id="closePopup"
-            onClick={() => setIsOpened(false)}
-            type="button"
-          >
-            Close
-          </button>
-        </div>
-        <div className={"flex justify-center items-center"}>
-          <div>{popupChildren}</div>
-        </div>
-      </dialog>
+      {isOpened ? <AutoPopup {...props} /> : ""}
     </div>
   );
 }
